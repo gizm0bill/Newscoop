8085722a68495a32e4b7183b9760694c62c423c0
petr.jasek <petr.jasek@sourcefabric.org>
2012-01-16 14:39:02 +0100

    WOBS-851: Image renditions
    Initial article place image screen.

--- newscoop/application/Bootstrap.php
+++ newscoop/application/Bootstrap.php
@@ -165,6 +165,9 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $container->register('tree', 'Newscoop\Tree\TreeService')
             ->addArgument(new sfServiceReference('em'));
 
+        $container->register('view.helper.thumbnail', 'Newscoop\Image\ThumbnailViewHelper')
+            ->addArgument(new sfServiceReference('image'));
+
         Zend_Registry::set('container', $container);
         return $container;
     }
