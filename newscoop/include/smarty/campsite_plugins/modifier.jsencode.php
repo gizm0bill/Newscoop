86c53307ee20cd500b266f46660ce836a67b3c6c
Sebastian Goebel <sebastian.goebel@sourcefabric.org>
2012-02-29 09:29:56 +0100

    WOBS-1040 Mobile site break by invalid chars

--- newscoop/include/smarty/campsite_plugins/modifier.jsencode.php
+++ newscoop/include/smarty/campsite_plugins/modifier.jsencode.php
@@ -12,32 +12,26 @@
  * Type:     modifier<br>
  * Name:     truncate<br>
  * Purpose:  Trim whitespace, encode, and optionally strip tages in string.
- * @author Sebastian Göbel <sebastian.goebel at sourcefabric dot org>
+ * @author Sebastian Gï¿½bel <sebastian.goebel at sourcefabric dot org>
  * @param string
  * @param boolean
  * @param boolean
  * @return string
  */
 
-function smarty_modifier_jsencode($string, $strip_tags = '<h4><b><i><em><strong><br><p><a><img>', $addslashes = true)
+function smarty_modifier_jsencode($string, $addslashes = false, $keep_tags = '<h4><b><i><em><strong><br><p><a><img>')
 {
-    //$string = iconv("UTF-8","UTF-8//IGNORE",$string); // http://www.zeitoun.net/articles/clear-invalid-utf8/start
-   $string = iconv("UTF-8","ISO-8859-1//TRANSLIT",$string);
-   $string = iconv("ISO-8859-1","UTF-8//TRANSLIT",$string);
-    //$string = preg_replace('/[^(\x20-\x7F)]*/','', $string);
-   //$string = utf8_encode($string);
+  if ($keep_tags) {
+    $string = strip_tags($string, $keep_tags);
+  }
+  
+  if ($addslashes) {
+    $string = addslashes($string);
+  }
+  
+  $string = substr(json_encode($string, JSON_HEX_APOS|JSON_HEX_QUOT), 1, -1);
 
-    $string = preg_replace("/\s+/", " ", $string);  // replace whitespace by single space
-    
-    if ($strip_tags) {
-      $string = strip_tags($string, $strip_tags);
-    }
-    
-    if ($addslashes) {
-      $string = addslashes($string);
-    }
-    
-    return $string;
+  return $string;
 } // smarty_modifier_jsencode
 
 ?>
