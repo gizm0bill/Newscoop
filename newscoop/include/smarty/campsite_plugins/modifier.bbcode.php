0047e2ef554d3df3705d9f9890fe6f0a19883d54
petr.jasek <petr.jasek@sourcefabric.org>
2012-03-19 11:04:08 +0100

    WOBS-662: HTML doesn't show on frontend
    Add bbcode smarty modifier.

---
+++ newscoop/include/smarty/campsite_plugins/modifier.bbcode.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package Newscoop
+ * @copyright 2012 Sourcefabric o.p.s.
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+/**
+ * Converts bbcodes to html
+ *
+ * @param string $input
+ * @return string
+ */
+function smarty_modifier_bbcode($input)
+{
+    static $bbcode;
+
+    if ($bbcode === null) {
+        $bbcode = \Zend_Markup::factory('Bbcode');
+    }
+
+    return $bbcode->render($input);
+}
