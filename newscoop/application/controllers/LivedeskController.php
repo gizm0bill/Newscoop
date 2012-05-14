dce222bdcf806a496c3bb16f8097d8d45722d074
petr.jasek <petr.jasek@sourcefabric.org>
2012-05-14 15:21:58 +0300

    NR-120: Integrate livedesk
    Send If-Modified-Since header from client and handle it on server.

--- newscoop/application/controllers/LivedeskController.php
+++ newscoop/application/controllers/LivedeskController.php
@@ -5,7 +5,7 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.txt
  */
 
-use Doctrine\Common\Cache\ArrayCache as Cache;
+use Doctrine\Common\Cache\ApcCache as Cache;
 use Guzzle\Http\Client;
 use Guzzle\Common\Cache\DoctrineCacheAdapter;
 use Guzzle\Http\Plugin\CachePlugin;
@@ -14,6 +14,11 @@ use Guzzle\Http\Plugin\CachePlugin;
  */
 class LivedeskController extends Zend_Controller_Action
 {
+    const IF_MODIFIED_SINCE_HEADER = 'If-Modified-Since';
+    const BLOG_POSTS_PATH = '/resources/LiveDesk/Blog/{id}/BlogPost/Published';
+    const BLOG_INFO_PATH = '/resources/LiveDesk/Blog/{id}';
+    const RFC1123_DATE = 'D, d M Y H:i:s \G\M\T';
+
     public function init()
     {
         $this->_helper->contextSwitch
@@ -25,26 +30,38 @@ class LivedeskController extends Zend_Controller_Action
     {
         try {
             $client = $this->getClient(1);
-            $blog = $client->get('/resources/LiveDesk/Blog/{id}')->send();
-            $posts = $client->get('/resources/LiveDesk/Blog/{id}/BlogPost/Published', array(
-                'X-Filter' => 'Content, PublishedOn, Creator.Name',
-            ))->send();
+            list($blog, $posts) = $client->send(array(
+                $client->get(self::BLOG_INFO_PATH),
+                $client->get(self::BLOG_POSTS_PATH, $this->getPostsHeaders()),
+            ));
+
+            $this->view->blog = json_decode($blog->getBody(TRUE));
+            $this->view->posts = $posts->getBody(TRUE);
         } catch (Guzzle\Http\Exception\BadResponseException $e) {
+            $this->getResponse()->setHttpResponseCode(500);
             echo 'Error!', ' ' , $e->getMessage();
         }
-
-        $this->view->blog = json_decode($blog->getBody(TRUE));
-        $this->view->posts = $posts->getBody(TRUE);
     }
 
     public function updateAction()
     {
-        $client = $this->getClient(1);
-        $posts = $client->get('/resources/LiveDesk/Blog/{id}/BlogPost/Published', array(
-            'X-Filter' => 'Content, PublishedOn, Creator.Name',
-        ))->send();
-
-        $this->_helper->json(json_decode($posts->getBody(TRUE)));
+        $this->_helper->viewRenderer->setNoRender();
+        try {
+            $client = $this->getClient(1);
+            $posts = $client->get(self::BLOG_POSTS_PATH, $this->getPostsHeaders())->send();
+            if (!$this->isUpdated($posts)) {
+                $this->getResponse()->setHttpResponseCode(304);
+                $this->getResponse()->clearBody();
+                $this->getResponse()->sendResponse();
+            } else {
+                $this->_helper->json(json_decode($posts->getBody(TRUE)));
+            }
+        } catch (Exception $e) {
+            $this->getResponse()->setHttpResponseCode(500);
+            $this->_helper->json(array(
+                'errorMessage' => $e->getMessage(),
+            ));
+        }
     }
 
     /**
@@ -60,9 +77,53 @@ class LivedeskController extends Zend_Controller_Action
         ));
 
         $adapter = new DoctrineCacheAdapter(new Cache());
-        $cache = new CachePlugin($adapter, true);
+        $cache = new CachePlugin($adapter, TRUE);
         $client->getEventDispatcher()->addSubscriber($cache);
 
         return $client;
+    }
+
+    /**
+     * Get headers for posts request
+     *
+     * @return array
+     */
+    private function getPostsHeaders()
+    {
+        return array_filter(array(
+            'X-Filter' => 'Content, PublishedOn, Creator.Name',
+            self::IF_MODIFIED_SINCE_HEADER => $this->getIfModifiedSinceHeader(),
+        ));
+    }
+
+    /**
+     * Test if posts are updated after last mod header
+     *
+     * @return bool
+     */
+    private function isUpdated($posts)
+    {
+        if (!$this->getIfModifiedSinceHeader()) {
+            return TRUE;
+        }
+
+        $items = json_decode($posts->getBody(TRUE))->BlogPostList;
+        if (empty($items)) {
+            return FALSE;
+        }
+
+        $now = new DateTime($this->getIfModifiedSinceHeader());
+        $lastPublished = new DateTime($items[0]->PublishedOn);
+        return $now->getTimestamp() <= $lastPublished->getTimestamp();
+    }
+
+    /**
+     * Get If-Modified-Since header
+     *
+     * @return string
+     */
+    private function getIfModifiedSinceHeader()
+    {
+        return $this->getRequest()->getHeader(self::IF_MODIFIED_SINCE_HEADER) ? $this->getRequest()->getHeader(self::IF_MODIFIED_SINCE_HEADER) : null;
     }
 }
