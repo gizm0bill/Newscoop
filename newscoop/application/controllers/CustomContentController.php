ea18df4ad22bcee6b65de05d3c2159b6c218ddae
Ozan Ozbek <ozan.ozbek@sourcefabric.org>
2012-05-22 12:46:45 +0200

    WOBS-1394: flipside: exclude advertisement from preloading
    Fixed.

---
+++ newscoop/application/controllers/CustomContentController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package Newscoop
+ * @copyright 2011 Sourcefabric o.p.s.
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+/**
+ */
+class CustomContentController extends Zend_Controller_Action
+{
+    public function backpageAdvertisementAction()
+    {
+        echo('<script type="text/javascript" src="http://imagesrv.adition.com/js/adition.js"></script><div id="adition_tag_520595"></div>');
+        die;
+    }
+}
