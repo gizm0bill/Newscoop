c6ffd67a665c919135afe111417c83b6618105c8
omniton <planetaz@gmail.com>
2012-05-23 19:20:13 +0300

    IN-129: Caching of movie section pages fixed insert cache sql query

--- newscoop/classes/cache/TemplateCacheHandler_DB.php
+++ newscoop/classes/cache/TemplateCacheHandler_DB.php
@@ -127,7 +127,11 @@ class TemplateCacheHandler_DB extends TemplateCacheHandler
                     $queryStr .= '(' . implode(',', array_keys($campsiteVector));
                     $queryStr .=  ',template,expired,content) VALUES (';
                     foreach ($campsiteVector as $key => $value) {
-                        $queryStr .= !isset($value) ? 'NULL,' : $value . ',';
+                        if ($key == 'params') {
+                            $queryStr .= !isset($value) ? 'NULL,' : "'" . addslashes($value) . "'" . ',';
+                        } else {
+                            $queryStr .= !isset($value) ? 'NULL,' : $value . ',';
+                        }
                     }
                     $queryStr .= "'$tpl_file',$exp_time,'" . addslashes($cache_content) . "')";
                     $g_ado_db->Execute($queryStr);
