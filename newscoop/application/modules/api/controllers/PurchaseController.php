61f860deb4fab92d22a4bef151e0296020f2c6b5
petr.jasek <petr.jasek@sourcefabric.org>
2012-06-12 15:26:26 +0200

    WOBS-1727: Purchase API
    Initial receipt verification.

--- newscoop/application/modules/api/controllers/PurchaseController.php
+++ newscoop/application/modules/api/controllers/PurchaseController.php
@@ -5,55 +5,38 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.txt
  */
 
+use Guzzle\Http\Client;
+
 /**
  * Purchage API
  */
 class Api_PurchaseController extends Zend_Controller_Action
 {
-    /** @var array */
-    private $methods = array(
-        'products' => 'GET',
-        'validate' => 'POST',
-        'free_upgrade' => 'GET',
-    );
+    const PRODUCTION_URL = 'https://buy.itunes.apple.com/verifyReceipt';
+    const SANDBOX_URL = 'https://sandbox.itunes.apple.com/verifyReceipt';
 
-    public function init()
-    {
-        $this->_helper->viewRenderer->setNoRender(true);
-    }
-
-    public function preDispatch()
-    {
-        $action = $this->getRequest()->getActionName();
-        if (!isset($this->methods[$action])) {
-            $this->getResponse()->setHttpResponseCode(400);
-            $this->getResponse()->clearBody();
-            $this->getResponse()->sendResponse();
-            exit;
-        }
-
-        if ($this->methods[$action] !== $this->getRequest()->getMethod()) {
-            $this->getResponse()->setHttpResponseCode(405);
-            $this->getResponse()->setHeader('Allow', $this->methods[$action]);
-            $this->getResponse()->clearBody();
-            $this->getResponse()->sendResponse();
-            exit;
-        }
-
-        if ($this->getRequest()->getMethod() === 'POST' && !$this->getRequest()->isSecure()) {
-            $this->getResponse()->setHttpResponseCode(400);
-            $this->getResponse()->clearBody();
-            $this->getResponse()->sendResponse();
-            exit;
-        }
-    }
+    const STATUS_SANDBOX = 21007;
 
     /**
      * Get list of products
      */
     public function productsAction()
     {
+        $expires = new DateTime('+300 day');
+        $lastMod = new DateTime(sprintf('@%d', filemtime(__FILE__)));
+        $this->getResponse()->setHeader('Expires', $expires->format(DateTime::RFC1123), true);
+        $this->getResponse()->setHeader('Last-Modified', $lastMod->format(DateTime::RFC1123), true);
+        $this->getResponse()->setHeader('Cache-Control', 'public', true);
+
         $this->_helper->json(array(
+            'single_issue' => 'ch.tageswoche.issue.2012.test',
+            'digital_upgrade' => 'ch.tageswoche.subscription.digital_upgrade',
+            'digital_subscriptions' => array(
+                'ch.tageswoche.subscription.month',
+                'ch.tageswoche.subscription.quarter',
+                'ch.tageswoche.subscription.bi_annual',
+                'ch.tageswoche.subscription.annual',
+            ),
         ));
     }
 
@@ -62,10 +45,44 @@ class Api_PurchaseController extends Zend_Controller_Action
      */
     public function validateAction()
     {
+        if (!$this->getRequest()->isSecure()) {
+            $this->sendError("Https required");
+        }
+
+        $data = $this->getData(self::PRODUCTION_URL);
+        if ($data->status === self::STATUS_SANDBOX) {
+            $data = $this->getData(self::SANDBOX_URL);
+        }
+
         $this->_helper->json(array(
-            'receipt_valid' => false,
-            'production_id' => uniqid(),
+            'receipt_valid' => $data->status === 0,
+            'product_id' => $data->status === 0 ? $data->receipt->product_id : null,
         ));
+    }
+
+    /**
+     * Get response for validate request
+     *
+     * @param string $url
+     * @return object
+     */
+    private function getData($url)
+    {
+        try {
+            $client = new Client();
+            $response = $client->post($url, null, json_encode(array(
+                'receipt-data' => $this->_getParam('receipt_data'),
+                'password' => $this->getInvokeArg('bootstrap')->getOption('itunes_shared_secret'),
+            )))->send();
+        } catch (\Exception $e) {
+            $this->sendError($e->getMessage(), 500);
+        }
+
+        if (!$response->isSuccessful()) {
+            $this->sendError($response->getStatusCode(), 500);
+        }
+
+        return json_decode($response->getBody(true));
     }
 
     /**
@@ -75,6 +92,22 @@ class Api_PurchaseController extends Zend_Controller_Action
     {
         $this->_helper->json(array(
             'status' => 'ok',
+        ));
+    }
+
+    /**
+     * Send error and exit
+     *
+     * @param string $body
+     * @param int $code
+     * @return void
+     */
+    private function sendError($body = '', $code = 400)
+    {
+        $this->getResponse()->setHttpResponseCode($code);
+        $this->_helper->json(array(
+            'code' => $code,
+            'message' => $body,
         ));
     }
 }
