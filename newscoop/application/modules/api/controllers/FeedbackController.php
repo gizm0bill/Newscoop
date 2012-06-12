c0ac34c5f6163a5584a7651a3effb790e8e15890
Ozan Ozbek <ozan.ozbek@sourcefabric.org>
2012-06-12 09:42:17 +0200

    WOBS-1731: Newscoop TagesWoche API - Feedback Service
    Done (except attachments)

--- newscoop/application/modules/api/controllers/FeedbackController.php
+++ newscoop/application/modules/api/controllers/FeedbackController.php
@@ -113,7 +113,6 @@ class Api_FeedbackController extends Zend_Controller_Action
     public function sendMail($values)
     {
         $toEmail = 'dienstpult@tageswoche.ch';
-        $toEmail = 'ozan.ozbek@sourcefabric.org';
         
         $userRepository = $this->getHelper('entity')->getRepository('Newscoop\Entity\User');
         $user = $userRepository->find($values['user']);
